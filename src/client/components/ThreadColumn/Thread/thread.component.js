948aba887a7eed77d46f5d30ce4cbb45bda222a9
Alexandra Paul <alexandra_paul@brown.edu>
2017-11-08 18:28:52 +0200

    starting to style the threads list

--- src/client/components/ThreadColumn/Thread/thread.component.js
+++ src/client/components/ThreadColumn/Thread/thread.component.js
@@ -1,11 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types"; // ES6
 
+const titleStyle = {
+  fontFamily: "AvenirNext-Bold",
+  fontSize: "26px",
+  color: "#4A4A4A",
+  lineHeight: "24px"
+};
+
+const dateStyle = {
+  fontFamily: "AvenirNext-Regular",
+  fontSize: "14px",
+  color: "#4A4A4A",
+  lineHeight: "32px"
+};
+
+const bodyStyle = {
+  fontFamily: "AvenirNext-Regular",
+  fontSize: "26px",
+  color: "#4A4A4A",
+  lineHeight: "32px"
+};
+
 const Thread = props => (
   <div>
-    <h3>{props.title}</h3>
-    <div>{props.text}</div>
-    <div>{props.date}</div>
+    <h3 style={titleStyle}>{props.title}</h3>
+    <div style={dateStyle}>{props.date}</div>
+    <div style={bodyStyle}>{props.text}</div>
   </div>
 );
 
