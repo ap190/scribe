6140d3b8016f2532607a261416dbe754ec1e54ca
Omer Goldberg <arieg419@gmail.com>
2017-11-12 16:51:52 +0200

    implemented basic modal functionality in Aside Component for create channel

--- src/client/components/AsideColumn/Channels/channels.component.js
+++ src/client/components/AsideColumn/Channels/channels.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "../Header/header.component";
 import "../aside.css";
 
@@ -7,14 +8,20 @@ const addChannelIcon = "./assets/icons/add_channel.png";
 const Channels = props => (
   <div className="channels">
     <div className="channelsTitleContainer">
-      <Header title="Channels" />
+      <Header title={props.title} />
       <img
         className="add-channel-icon"
         src={addChannelIcon}
         alt="Add a channel button"
+        onClick={props.createChannelHandler}
       />
     </div>
   </div>
 );
+
+Channels.propTypes = {
+  title: PropTypes.string.isRequired,
+  createChannelHandler: PropTypes.func.isRequired
+};
 
 export default Channels;
