c5b526bf4d20a53b533c151013a93d9248ec0a41
Alexandra Paul <alexandra_paul@brown.edu>
2017-12-24 21:41:28 +0200

    styling on user avatar and info

--- src/client/components/common/UserDetails/user-details.component.js
+++ src/client/components/common/UserDetails/user-details.component.js
@@ -4,7 +4,7 @@ import "./user-details.css";
 
 const UserDetails = props => (
   <div className="userDetailsContainer">
-    <div>{`${props.firstName} ${props.lastName}`}</div>
+    <div className="full-name">{`${props.firstName} ${props.lastName}`}</div>
     <div>{props.email}</div>
   </div>
 );
