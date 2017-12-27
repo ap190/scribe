50f2949714780d5881499e8532b374be5e4cb1e5
Omer Goldberg <arieg419@gmail.com>
2017-12-27 19:51:00 +0200

    refsactor single file

--- src/client/components/HomePage/file-storage.api.js
+++ src/client/components/HomePage/file-storage.api.js
@@ -1,5 +1,23 @@
 import { saveWorkspace, saveFile } from "./ipcRenderer.api";
 
-export const saveFile = () => {};
+export const saveSingleFile = componentContext => {
+  // Get Current File
+  if (!componentContext.state.currentThread) return;
+  const { channelId, id } = componentContext.state.currentThread;
 
-export const saveWorkspace = () => {};
+  componentContext.saveWorkspace();
+
+  // Read Current File from Map
+  const newDocTitle = `${channelId}****${id}`;
+
+  // Remove saved Doc from Cache, perhaps this should be done via setState?
+  componentContext.state.unsavedDocCache.delete(newDocTitle);
+
+  componentContext.setState({
+    wasDocumentEdited: false
+  });
+};
+
+export const saveAllFiles = componentContext => {
+  console.log(componentContext);
+};
